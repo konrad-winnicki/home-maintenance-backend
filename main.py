e7db5259d157886b696416c1cad62bb49dc36d3b
Konrad Winnicki <konrad.winnicki@protonmail.com>
2023-03-18 22:35:42 +0100

    token session code v3

--- main.py
+++ main.py
@@ -376,7 +376,7 @@ def create_tables():
 def authorization_verification(token):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms="HS256")
-        user_id = payload.user_id
+        user_id = payload.get('user_id')
         return user_id
     except jwt.ExpiredSignatureError:
         print("non valid token")

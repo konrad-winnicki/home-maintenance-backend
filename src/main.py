912b42b4634f1fd482ea8bcaba5d128e0f8ad875
Michal Grabarczyk <michal.grabarczyk@gmail.com>
2024-01-04 22:53:25 +0100

    fix: start web sockets also when run locally with werkzeug

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 
 from src.db_schema import create_tables
-from src.api import app
+from src.api import app, socketio
 
 create_tables()
 
@@ -9,11 +9,4 @@ app_profile = os.getenv('APP_PROFILE')
 on_local_environment = app_profile == 'local'
 if on_local_environment:
     print('Starting development server on local environment')
-
-    app.run(
-        host="0.0.0.0",
-        debug=True,
-        #ssl_context=('../cert/cert.pem', '../cert/key.pem')
-    )
-
-    #socketio.run(app, allow_unsafe_werkzeug=True)
+    socketio.run(app, allow_unsafe_werkzeug=True)
